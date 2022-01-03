63babe0de08ffa4c52627af0fc267f1dd624ad22
teamschmid <info@teamschmid.com>
2022-01-03 18:58:31 +0100

    Erste Version

---
+++ git.ps1
@@ -0,0 +1,24 @@
+…or create a new repository on the command line
+git init
+git add git.ps1
+git commit -m "first commit"
+git branch -M main
+git commit -m https://github.com/teamschmid/Script.git
+
+git remote add origin https://github.com/teamschmid/Script.git
+git push -u origin main
+
+
+…or push an existing repository from the command line
+ git remote add origin https://github.com/teamschmid/Script.git
+git branch -M main
+git push -u origin main
+
+
+
+
+git --version
+
+#im git-Pfad direkt unter Hauptverzeichnis ausführen, um Inhalte lokal zu speichern
+git clone https://github.com/teamschmid/Script.git
+
